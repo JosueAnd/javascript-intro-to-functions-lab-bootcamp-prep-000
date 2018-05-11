1cef08952fccf66f1f085adc9a38fba9c1619ea4
Joshua Andujar <andujar0218@gmail.com>
2018-05-11 04:10:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,14 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(sting) {
+function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
+  }
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
+  }
   if (string === "I love you, Grandma.") {
     return "I love you, too."
+  }
 }
