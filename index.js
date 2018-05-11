888e036a523f6774dc23a9a66cb1144fe3218891
Joshua Andujar <andujar0218@gmail.com>
2018-05-11 04:06:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function sayHiToGrandma(sting) {
     return "I can't hear you!"}
   if (string.toUpperCase() === string) {
     return "YES INDEED!"}
-  else if (string === "I love you, Grandma.") {
+  if (string === "I love you, Grandma.") {
     return "I love you, too."}
 }
